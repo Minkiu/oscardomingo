7b664b8fd77c5855c9582836141b2db58729689c
Minkiu <oscardomingo@gmail.com>
2016-09-28 23:04:30 +0100

    Initial layout of the three pages, with their own elements

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
 
+$(".block__works").each(function() {
+  var imageUrl = $(this).attr("data-image-src");
+
+  if (typeof imageUrl !== typeof undefined && imageUrl !== false) {
+    $(this).css('background', 'url('+imageUrl+') center no-repeat');
+    $(this).css('background-size', 'cover');
+  }
+
+
+});
